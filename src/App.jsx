839a1946ff4942ec696a2001e327d4c6e7498b81
Ayudh <ayudhnandi@gmail.com>
2024-08-27 16:30:21 +0530

    added code for splashscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import Home from "./Components/Home";
 import DisplayDateTime from "./Components/DateTime";
+import SplashScreen from "./Components/SplashScreen";
+
+import { useState } from "react";
 
 import "./Components/style.css";
 
-function App() {
+const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  const updateShowSplash = () => {
+    setShowSplash(false);
+  };
+
   return (
     <>
+      {showSplash && <SplashScreen onAnimationEnd={updateShowSplash} />}
       <DisplayDateTime />
       <Home />
     </>
   );
-}
+};
 
 export default App;
