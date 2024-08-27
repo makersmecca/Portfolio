aa1a88172ee190b38b06ed7cd8dec093fe3f132b
makersmecca <ayudhnandi@gmail.com>
2024-08-27 20:02:50 +0530

    added splashcreen transform transition animation to move up

--- src/Components/SplashScreen.jsx
+++ src/Components/SplashScreen.jsx
@@ -1,17 +1,36 @@
 import { useEffect } from "react";
+import { useState } from "react";
 const SplashScreen = ({ onAnimationEnd }) => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const timer = setTimeout(() => {
-      onAnimationEnd();
-    }, 3000);
+      setLoading(false);
+    }, 2500);
 
     return () => clearTimeout(timer);
-  }, [onAnimationEnd]);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      // Delay the callback to allow the transition to complete
+      const transitionTimer = setTimeout(() => {
+        onAnimationEnd(); // Hide the splash screen fully after the transition
+      }, 1000);
+
+      return () => clearTimeout(transitionTimer);
+    }
+  }, [loading, onAnimationEnd]);
+
   return (
-    <div className="relative">
-      <div className="h-screen p-10 bg-black absolute font-Kanit w-full text-center text-center">
-        <div className="text-5xl animate-bounce">WELCOME</div>
-      </div>
+    <div
+      className={`fixed inset-0 bg-black flex items-center justify-center transition-transform duration-1000 ${
+        !loading ? "-translate-y-full" : "translate-y-0"
+      }`}
+    >
+      <h1 className="text-white font-Kanit text-4xl font-bold animate-pulse">
+        WELCOME
+      </h1>
     </div>
   );
 };
