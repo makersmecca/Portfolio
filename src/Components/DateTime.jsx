5fca31b4a1479dd39b22d3f9ab3aa068b2efbe28
makersmecca <ayudhnandi@gmail.com>
2024-08-27 20:26:57 +0530

    formatted date and time display

--- src/Components/DateTime.jsx
+++ src/Components/DateTime.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 const DisplayDateTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
-
   function updateTime() {
     setCurrentTime((c) => new Date());
   }
@@ -9,15 +8,33 @@ const DisplayDateTime = () => {
   //console.log(currentTime);
 
   setInterval(updateTime, 1000);
+
   return (
     <>
       <div className="py-5">
         <h5 className="font-Kanit text-center">
-          <span className="text-6xl">{`${currentTime.getHours()}:${currentTime.getMinutes()}`}</span>
+          <span className="text-6xl">
+            {currentTime.getHours() < 10 ? (
+              <span>0{currentTime.getHours()}</span>
+            ) : (
+              <span>{currentTime.getHours()}</span>
+            )}
+            :
+            {currentTime.getMinutes() < 10 ? (
+              <span>0{currentTime.getMinutes()}</span>
+            ) : (
+              <span>{currentTime.getMinutes()}</span>
+            )}
+          </span>
+          {/* <span className="text-6xl">{`${currentTime.getHours()}:${currentTime.getMinutes()}`}</span> */}
           {/* {currentTime.toTimeString().substring(0, 8)} */}
         </h5>
-        <h5 className="font-Kanit text-3xl font-semibold text-center">
-          {currentTime.toDateString().substring(3)}
+        <h5 className="font-Kanit text-2xl font-semibold text-center">
+          <span>
+            {currentTime.toDateString().substring(0, 3)},{" "}
+            {currentTime.toDateString().substring(8, 10)}{" "}
+            {currentTime.toDateString().substring(4, 7)}
+          </span>
         </h5>
       </div>
     </>
