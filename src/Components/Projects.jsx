e52e6d309b25011bafbafae3d0b897bc58c83907
Ayudh <ayudhnandi@gmail.com>
2024-08-28 16:20:45 +0530

    component containing array fo obejcts with details of each project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,35 @@
+import MyProjectDetails from "./MyProjectDetails";
 const Projects = () => {
+  const projectDetails = [
+    {
+      img: "https://github.com/makersmecca/Project-ShopSum/raw/master/ShopSum-poster.png",
+      name: "ShopSum1",
+      details: "Payments Simplified",
+      url: "https://github.com/makersmecca/Project-ShopSum",
+    },
+    {
+      img: "/",
+      name: "ShopSum2",
+      details: "Payments Simplified",
+      url: "www.github.com",
+    },
+    {
+      img: "/",
+      name: "ShopSum3",
+      details: "Payments Simplified",
+      url: "www.github.com",
+    },
+    {
+      img: "/",
+      name: "ShopSum4",
+      details: "Payments Simplified",
+      url: "https://www.github.com",
+    },
+  ];
+
   return (
     <>
-      <div>Projects</div>
+      <MyProjectDetails projectDetails={projectDetails} />
     </>
   );
 };
