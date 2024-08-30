88c8396498631eb8cc1753a40f6fbbc327d22f0b
Ayudh <ayudhnandi@gmail.com>
2024-08-30 13:10:57 +0530

    added animation exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily:{
-      Kanit: ["Kanit", "sans-serif"]
-    }
+    extend: {
+      keyframes: {
+        wobble: {
+          "0%": { borderRadius: "40% 60% 60% 40%", transform: "scale(1)" },
+          "20%": { borderRadius: "55% 45% 60% 40%", transform: "scale(1.05)" },
+          "40%": { borderRadius: "60% 40% 500% 50%", transform: "scale(0.95)" },
+          "60%": { borderRadius: "50% 60% 40% 60%", transform: "scale(1.1)" },
+          "80%": { borderRadius: "65% 35% 55% 45%", transform: "scale(0.95)" },
+          "100%": { borderRadius: "50% 50% 50% 50%", transform: "scale(1)" },
+        },
+        animation: {
+          wobble: "wobble 10s infinite cubic-bezier(0.68, -0.55, 0.27, 1.55)",
+        },
+      },
+    },
+    fontFamily: {
+      Kanit: ["Kanit", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
