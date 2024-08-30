22be1164218f1969c27637d16a070ce847c6688e
Ayudh <ayudhnandi@gmail.com>
2024-08-30 16:43:54 +0530

    updated md breakpoint for margin top

--- src/Components/MyProjectDetails.jsx
+++ src/Components/MyProjectDetails.jsx
@@ -7,7 +7,7 @@ const MyProjectDetails = ({ projectDetails }) => {
 
   return (
     <>
-      <div className="mt-20 pt-5 grid grid-cols-2 md:grid-cols-5 xl:px-80 lg:px-52 md:px-36 px-8">
+      <div className="mt-10 md:mt-20 pt-5 grid grid-cols-2 md:grid-cols-5 xl:px-80 lg:px-52 md:px-36 px-8">
         <div className="col-span-2 col-start-1 text-center md:text-start md:ps-16 md:me-9 text-3xl md:text-4xl">
           Projects
         </div>
