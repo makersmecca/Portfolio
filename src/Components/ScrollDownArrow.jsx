f220d0eceb05dc2018e68b38dbfae6a29a2f5b48
makersmecca <ayudhnandi@gmail.com>
2024-09-01 01:46:19 +0530

    added scale on hover animation

--- src/Components/ScrollDownArrow.jsx
+++ src/Components/ScrollDownArrow.jsx
@@ -29,7 +29,7 @@ const ScrollDownArrow = ({ splashStatus }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="white"
             viewBox="0 0 512 512"
-            className="cursor-pointer w-6 h-6 md:w-7 md:h-7"
+            className="cursor-pointer w-6 h-6 md:w-7 md:h-7 hover:scale-110"
             onClick={scrollDwn}
           >
             <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
