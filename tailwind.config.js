07795eaeb6f6eafd457b79ae24ff2671374b7598
makersmecca <ayudhnandi@gmail.com>
2024-08-31 17:02:48 +0530

    added custom animation to be added to intro div for typing animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darktheme: "#0d0d0d",
+      },
+      animation: {
+        typing: "typing 2s steps(11), blink 1s infinite",
+      },
+      keyframes: {
+        typing: {
+          from: {
+            width: "0",
+          },
+          to: {
+            width: "3ch",
+          },
+        },
+        blink: {
+          from: {
+            "border-color": "black",
+          },
+          to: {
+            "border-color": "white",
+          },
+        },
+      },
+    },
     fontFamily: {
       Kanit: ["Kanit", "sans-serif"],
     },
