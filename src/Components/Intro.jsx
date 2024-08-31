ad07f762b144dfd43eac5c6736132a44a78546ae
makersmecca <ayudhnandi@gmail.com>
2024-08-31 14:45:37 +0530

    centered intro div vertically wrt to the screen height

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,35 +3,23 @@ import DisplayPicture from "./DisplayPicture";
 const Intro = ({ isAnimated }) => {
   return (
     <>
-      {/* <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-        <div className="grid-cols-12">
-          <h2 className="text-3xl">Photo</h2>
-        </div>
-        <div className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
-          <h1 className="text-3xl">Display Name</h1>
-        </div>
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quos
-            dolores corporis velit nam. Voluptate sed laudantium architecto,
-            nostrum eum animi ipsam quas iure minus, dicta magnam officia enim
-            obcaecati?
-          </p>
-        </div>
-      </div> */}
-
-      <div className="pt-5 grid grid-cols-2 md:grid-cols-5 xl:px-80 lg:px-52 md:px-36 px-8">
-        <div className="col-span-2 flex justify-center mt-12 md:mt-0">
-          <DisplayPicture isAnimated={isAnimated} />
-        </div>
-        <div className="col-span-2 text-center flex justify-center items-center mt-12 md:mt-0">
-          <h2 className="md:text-5xl text-3xl">AYUDH</h2>
-        </div>
-        <div className="col-span-3 md:col-start-2 text-center mt-12 lg-pt-12 px-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quos
-          dolores corporis velit nam. Voluptate sed laudantium architecto,
-          nostrum eum animi ipsam quas iure minus, dicta magnam officia enim
-          obcaecati?
+      <div className="flex flex-col min-h-screen">
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="grid grid-cols-2 md:grid-cols-5 xl:px-80 lg:px-52 md:px-36 px-8">
+            <div className="col-span-2 flex justify-center mt-12 md:mt-0">
+              <DisplayPicture isAnimated={isAnimated} />
+            </div>
+            <div className="col-span-2 text-center flex justify-center items-center mt-12 md:mt-0">
+              <h2 className="md:text-5xl text-3xl">AYUDH</h2>
+            </div>
+            <div className="col-span-3 md:col-start-2 text-center mt-12 lg-pt-12 px-10 md:text-xl">
+              Frontend developer with experience in building responsive,
+              user-friendly websites & progressive web apps. Proficient in React
+              JS.
+              <br />
+              Eager to contribute and grow in a collaborative team.
+            </div>
+          </div>
         </div>
       </div>
     </>
