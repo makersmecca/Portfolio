1e6031de4191d9eb866a66bbe542ccb66252ff98
makersmecca <ayudhnandi@gmail.com>
2024-09-01 01:45:37 +0530

    passed splashStatus prop to DisplayPicture component

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -5,12 +5,14 @@ import ScrollDownArrow from "./ScrollDownArrow";
 const Intro = ({ splashStatus }) => {
   //console.log(splashStatus);
   return (
-    <>
+    <div>
       <div className="flex flex-col min-h-screen">
         <div className="flex justify-center items-center min-h-screen">
           <div className="grid grid-cols-2 md:grid-cols-5 xl:px-80 lg:px-52 md:px-36 px-8">
-            <div className="col-span-2 md:row-span-3 flex justify-center md:items-center mt-12 md:mt-0">
-              <DisplayPicture />
+            <div
+              className={`col-span-2 md:row-span-3 flex justify-center md:items-center mt-12 md:mt-0`}
+            >
+              <DisplayPicture splashStatus={splashStatus} />
             </div>
             <div className="col-span-2 md:col-span-3 md:col-start-3 text-center md:text-start flex md:justify-start justify-center items-center mt-12 px-10 md:mt-0">
               <div
@@ -39,7 +41,7 @@ const Intro = ({ splashStatus }) => {
         </div>
         <ScrollDownArrow splashStatus={splashStatus} />
       </div>
-    </>
+    </div>
   );
 };
 
