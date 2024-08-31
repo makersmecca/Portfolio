10475bf429b537c20f3172a0a3bd65709b65e499
makersmecca <ayudhnandi@gmail.com>
2024-09-01 01:44:54 +0530

    added glow effect to image background and animated it using appear and blob animations

--- src/Components/DisplayPicture.jsx
+++ src/Components/DisplayPicture.jsx
@@ -6,15 +6,15 @@ const DisplayPicture = ({ splashStatus }) => {
     <div>
       <div
         className={`${
-          splashStatus ? "hidden" : "absolute animate-appear"
-        } blob md:h-64 md:w-64 h-44 w-44 bg-slate-800 blur-3xl`}
+          splashStatus ? "hidden" : "absolute blob-fadein"
+        } md:h-64 md:w-64 h-44 w-44 bg-slate-800 blur-xl`}
       ></div>
       <img
         src={dispPic}
         alt="profile picture"
         className={`${
           splashStatus ? "" : "relative"
-        } blob md:h-64 md:w-64 h-44 w-44`}
+        } animate-blob md:h-64 md:w-64 h-44 w-44`}
       />
     </div>
   );
