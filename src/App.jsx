798cd3915e4d68c036cd9d2aa24a5b4780385036
makersmecca <ayudhnandi@gmail.com>
2024-09-01 16:01:04 +0530

    added a new state and effect to keep track of scroll postion and pass state to Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SplashScreen from "./Components/SplashScreen";
 import Socials from "./Components/Socials";
 import TechStack from "./Components/TechStack";
 import Projects from "./Components/Projects";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./Components/style.css";
 
@@ -15,12 +15,26 @@ const App = () => {
     setShowSplash(false);
   };
 
+  // this bit is used to implement the scroll down function of the arrow down button
+  const [scrollClicked, setScrollClicked] = useState(false); //sets the state to false showing that the page hasn't been scrolled
+
+  useEffect(() => {
+    const scrollDetect = () => {
+      setScrollClicked(window.scrollY <= 90 ? false : ""); //if the page is scrolled to top set state to false again
+    };
+    window.addEventListener("scroll", scrollDetect);
+    //console.log(isVisible);
+  });
+
+  const scrollHandler = () => {
+    setScrollClicked(true);
+  };
   return (
     <>
       {showSplash && <SplashScreen onAnimationEnd={updateShowSplash} />}
       <DisplayDateTime />
-      <Intro splashStatus={showSplash} />
-      <Projects />
+      <Intro splashStatus={showSplash} scrollHandler={scrollHandler} />
+      <Projects scrollClicked={scrollClicked} />
       <TechStack />
       <Socials />
     </>
