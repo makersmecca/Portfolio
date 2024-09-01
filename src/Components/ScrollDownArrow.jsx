9a4dd88fcdb5ae502f28f6d2d1b04b3f60fa8718
makersmecca <ayudhnandi@gmail.com>
2024-09-01 16:04:03 +0530

    takes the scrollHandler prop from Intro and implements it on Click

--- src/Components/ScrollDownArrow.jsx
+++ src/Components/ScrollDownArrow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-const ScrollDownArrow = ({ splashStatus }) => {
+const ScrollDownArrow = ({ splashStatus, btnClicked }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
@@ -9,10 +9,6 @@ const ScrollDownArrow = ({ splashStatus }) => {
     window.addEventListener("scroll", scrollDetect);
     //console.log(isVisible);
   });
-
-  const scrollDwn = () => {
-    console.log("scrolling down aaaaa!!!!");
-  };
 
   return (
     <>
@@ -24,17 +20,17 @@ const ScrollDownArrow = ({ splashStatus }) => {
         ${isVisible ? "" : "ease-in-out hidden"}
         `}
       >
-        <div className="text-3xl text-gray-500 animate-bounce">
+        <button className="text-3xl text-gray-500 animate-bounce">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="white"
             viewBox="0 0 512 512"
             className="cursor-pointer w-6 h-6 md:w-7 md:h-7 hover:scale-110"
-            onClick={scrollDwn}
+            onClick={btnClicked}
           >
             <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
           </svg>
-        </div>
+        </button>
       </div>
     </>
   );
