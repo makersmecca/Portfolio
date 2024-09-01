c6cf023e6f6e18c2bb8eeb68a310b90064e845a3
makersmecca <ayudhnandi@gmail.com>
2024-09-01 18:18:46 +0530

    updated padding

--- src/Components/DateTime.jsx
+++ src/Components/DateTime.jsx
@@ -11,7 +11,7 @@ const DisplayDateTime = () => {
 
   return (
     <>
-      <div className="md:ps-10 ps-5 text-start">
+      <div className="px-2">
         {currentTime.getHours() < 10 ? (
           <span>0{currentTime.getHours()}</span>
         ) : (
@@ -23,38 +23,12 @@ const DisplayDateTime = () => {
         ) : (
           <span>{currentTime.getMinutes()}</span>
         )}
-        <span className="md:ps-10 ps-5">
+        <span className="px-2">
           {currentTime.toDateString().substring(0, 3)},{" "}
           {currentTime.toDateString().substring(8, 10)}{" "}
           {currentTime.toDateString().substring(4, 7)}
         </span>
       </div>
-
-      {/* <div className="pt-7 mt-6 lg:pt-28 lg:mb-20">
-        <h5 className="font-Kanit flex justify-center">
-          <span className="md:text-6xl text-3xl">
-            {currentTime.getHours() < 10 ? (
-              <span>0{currentTime.getHours()}</span>
-            ) : (
-              <span>{currentTime.getHours()}</span>
-            )}
-            :
-            {currentTime.getMinutes() < 10 ? (
-              <span>0{currentTime.getMinutes()}</span>
-            ) : (
-              <span>{currentTime.getMinutes()}</span>
-            )}
-          </span>
-        </h5>
-
-        <h5 className="font-Kanit md:text-2xl font-semibold flex justify-center">
-          <span>
-            {currentTime.toDateString().substring(0, 3)},{" "}
-            {currentTime.toDateString().substring(8, 10)}{" "}
-            {currentTime.toDateString().substring(4, 7)}
-          </span>
-        </h5>
-      </div> */}
     </>
   );
 };
