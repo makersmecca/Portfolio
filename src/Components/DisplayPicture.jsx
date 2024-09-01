2c18b52e507213f15f32cf5627190323b6d1aa8a
makersmecca <ayudhnandi@gmail.com>
2024-09-01 13:47:51 +0530

    applying fix

--- src/Components/DisplayPicture.jsx
+++ src/Components/DisplayPicture.jsx
@@ -6,7 +6,9 @@ const DisplayPicture = ({ splashStatus }) => {
     <div>
       <div
         className={`${
-          splashStatus ? "hidden" : "absolute shadowBlobFadeIn"
+          splashStatus
+            ? "hidden"
+            : "absolute shadowBlobFadeIn transition-all ease-in-out duration-500"
         } md:h-64 md:w-64 h-44 w-44 bg-slate-800 blur-xl`}
       ></div>
       <img
