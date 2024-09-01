5c3f23978876f89a770a90858e12c73567ffaa12
makersmecca <ayudhnandi@gmail.com>
2024-09-01 13:37:18 +0530

    added css animation for blur glow

--- src/Components/DisplayPicture.jsx
+++ src/Components/DisplayPicture.jsx
@@ -6,7 +6,7 @@ const DisplayPicture = ({ splashStatus }) => {
     <div>
       <div
         className={`${
-          splashStatus ? "hidden" : "absolute blob-fadein"
+          splashStatus ? "hidden" : "absolute shadowBlobFadeIn"
         } md:h-64 md:w-64 h-44 w-44 bg-slate-800 blur-xl`}
       ></div>
       <img
