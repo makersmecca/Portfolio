a1cdf2ac0d4cf83883089fe8834a80358a1058d6
makersmecca <ayudhnandi@gmail.com>
2024-09-03 02:30:17 +0530

    updated poster images with compressed files

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -12,27 +12,27 @@ const Projects = ({ scrollClicked }) => {
 
   const projectDetails = [
     {
-      img: "https://github.com/makersmecca/Project-ShopSum/raw/master/ShopSum-poster.png",
+      img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/shopsumPoster.png",
       name: "Shop Sum",
       details:
         "Payment App for Small Vendors, Roadside Stalls and Humble Kirana (Grocery) Shops",
       url: "https://github.com/makersmecca/Project-ShopSum",
     },
     {
-      img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/portfolio-poster.png",
+      img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/portfolioPoster.png",
       name: "Portfolio",
       details: "My personal portfolio built with Vite-ReactJS and Tailwind CSS",
       url: "https://github.com/makersmecca/Portfolio",
     },
     {
-      img: "https://raw.githubusercontent.com/makersmecca/wireless_charger_project/ayudhNandi/wirelesscharger-poster.png",
+      img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/wirelessChargerPoster.png",
       name: "Wireless Charger project",
       details:
         "Companion PWA for Wireless Charger project to monitor battery charge status",
       url: "https://github.com/makersmecca/wireless_charger_project",
     },
     {
-      img: "https://raw.githubusercontent.com/makersmecca/HID-Knob/master/hidKnob_Schematics2.png.png",
+      img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/hidknobPoster.png",
       name: "HID Knob",
       details: "Multifunction Customizable Controller for Personal Computers",
       url: "https://github.com/makersmecca/HID-Knob",
