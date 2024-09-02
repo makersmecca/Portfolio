020f3fb402efc012d589ad010eb90468c086ef87
makersmecca <ayudhnandi@gmail.com>
2024-09-03 00:50:35 +0530

    added project details

--- src/Components/MyProjectDetails.jsx
+++ src/Components/MyProjectDetails.jsx
@@ -19,27 +19,15 @@ const MyProjectDetails = ({ projectDetails }) => {
               <li key={i.name} className="md:pt-7 pt-5">
                 <a
                   href={i.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                 >
-                  {/* <p>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="180"
-                      height="30"
-                      fill="currentColor"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
-                      <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54L1 12.5v-9a.5.5 0 0 1 .5-.5z" />
-                    </svg>
-                  </p> */}
                   <img src={i.img} alt="banner" className="mb-3 rounded-lg" />
                   <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {i.name}
                   </h5>
-                  <p className="font-normal text-center text-sm text-gray-700 dark:text-gray-400">
-                    {i.details}
-                  </p>
+                  <p className="font-normal text-center text-sm">{i.details}</p>
                 </a>
               </li>
             ))}
@@ -52,21 +40,12 @@ const MyProjectDetails = ({ projectDetails }) => {
             {arr2.map((i) => (
               <li key={i.name} className="md:pt-7 pt-5">
                 <a
-                  href="#"
+                  href={i.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
                 >
-                  <p>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="180"
-                      height="30"
-                      fill="currentColor"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
-                      <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2zm13 1a.5.5 0 0 1 .5.5v6l-3.775-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12v.54L1 12.5v-9a.5.5 0 0 1 .5-.5z" />
-                    </svg>
-                  </p>
+                  <img src={i.img} alt="banner" className="mb-3 rounded-lg" />
                   <h5 className="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {i.name}
                   </h5>
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -13,21 +13,22 @@ const Projects = ({ scrollClicked }) => {
   const projectDetails = [
     {
       img: "https://github.com/makersmecca/Project-ShopSum/raw/master/ShopSum-poster.png",
-      name: "ShopSum1",
-      details: "Payments Simplified",
+      name: "Shop Sum",
+      details: "Payment App for Small Businesses",
       url: "https://github.com/makersmecca/Project-ShopSum",
+    },
+    {
+      img: "https://raw.githubusercontent.com/makersmecca/wireless_charger_project/ayudhNandi/wirelesscharger-poster.png",
+      name: "Wireless Charger project",
+      details:
+        "Companion PWA for Wireless Charger project to monitor battery charge status",
+      url: "https://github.com/makersmecca/wireless_charger_project",
     },
     {
       img: "https://raw.githubusercontent.com/makersmecca/Portfolio/main/public/images/portfolio-poster.png",
       name: "Portfolio",
       details: "My personal portfolio built with React",
       url: "https://github.com/makersmecca/Portfolio",
-    },
-    {
-      img: "/",
-      name: "ShopSum3",
-      details: "Payments Simplified",
-      url: "www.github.com",
     },
     {
       img: "/",
