fd82ad12471fd7a778cd7f42e3c49b970e4ef0db
makersmecca <ayudhnandi@gmail.com>
2024-09-04 23:18:19 +0530

    added minor margin and padding

--- src/Components/DownloadResume.jsx
+++ src/Components/DownloadResume.jsx
@@ -10,10 +10,10 @@ const DownloadResume = () => {
         Resume
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="20"
+          width="25"
+          height="25"
           fill="currentColor"
-          className="bi bi-file-earmark-arrow-down ms-1"
+          className="bi bi-file-earmark-arrow-down ms-1 pt-1 me-1"
           viewBox="0 0 16 16"
         >
           <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
