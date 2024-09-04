f4710038ef7aea3d880fcfbc16e6c02aa88399fb
Ayudh <ayudhnandi@gmail.com>
2024-09-04 13:10:22 +0530

    added state variables and function to pass as prop for scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import SplashScreen from "./Components/SplashScreen";
 import Socials from "./Components/Socials";
 import TechStack from "./Components/TechStack";
 import Projects from "./Components/Projects";
+import BackToTop from "./Components/BackToTop";
+
 import { useState, useEffect } from "react";
 
 import "./Components/style.css";
@@ -17,9 +19,14 @@ const App = () => {
   // this bit is used to implement the scroll down function of the arrow down button
   const [scrollClicked, setScrollClicked] = useState(false); //sets the state to false showing that the page hasn't been scrolled
 
+  const [backToTop, setBackToTop] = useState(false);
+  let scrollPos = 0;
+
   useEffect(() => {
     const scrollDetect = () => {
-      setScrollClicked(window.scrollY <= 90 ? false : ""); //if the page is scrolled to top set state to false again
+      scrollPos = window.scrollY;
+      setScrollClicked(scrollPos <= 90 ? false : ""); //if the page is scrolled to top set state to false again
+      setBackToTop(scrollPos >= 1000 ? false : "");
     };
     window.addEventListener("scroll", scrollDetect);
     //console.log(isVisible);
@@ -27,6 +34,11 @@ const App = () => {
 
   const scrollHandler = () => {
     setScrollClicked(true);
+  };
+
+  const toTheTop = () => {
+    //console.log("app function");
+    setBackToTop(true);
   };
 
   const [isGlobalBlur, setIsGlobalBlur] = useState(false);
@@ -38,7 +50,9 @@ const App = () => {
   return (
     <>
       {showSplash && <SplashScreen onAnimationEnd={updateShowSplash} />}
+      <BackToTop toTheTop={toTheTop} splashStatus={showSplash} />
       <Intro
+        backToTop={backToTop}
         splashStatus={showSplash}
         scrollHandler={scrollHandler}
         globalBlur={blurStatus}
