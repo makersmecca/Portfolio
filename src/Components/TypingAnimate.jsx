bb0d8e7341aca454fd04553c6973349e9894922e
Ayudh <ayudhnandi@gmail.com>
2024-09-09 15:11:56 +0530

    trying out different caret animation and styles

--- src/Components/TypingAnimate.jsx
+++ src/Components/TypingAnimate.jsx
@@ -1,6 +1,13 @@
+// import { useState } from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const TypingAnimate = ({ splashStatus }) => {
+  //   const [cursorChar, setCursorChar] = useState(true);
+
+  //   setTimeout(() => {
+  //     setCursorChar(() => !cursorChar);
+  //   }, 400);
+
   return (
     <div
       className={`${
@@ -10,7 +17,7 @@ const TypingAnimate = ({ splashStatus }) => {
       <TypeAnimation
         sequence={[
           "Hi...", // Types 'One'
-          2500,
+          3000,
           "Hello...", // Deletes 'One' and types 'Two'
           1500,
           "Hi there...", // Types 'Three' without deleting 'Two'
@@ -21,6 +28,9 @@ const TypingAnimate = ({ splashStatus }) => {
         repeat={Infinity}
         style={{ fontSize: "2em", display: "inline-block" }}
       />
+      {/* <span style={{ fontSize: "1.8em" }}>
+        {cursorChar ? <span>/.</span> : <span>\.</span>}
+      </span> */}
     </div>
   );
 };
