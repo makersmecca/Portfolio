3fda9fe1c40a02e9891c74168562aedd2a677089
makersmecca <ayudhnandi@gmail.com>
2024-09-18 03:08:01 +0530

    trying to fix translucent box issue on ios browsers

--- src/Components/DisplayPicture.jsx
+++ src/Components/DisplayPicture.jsx
@@ -3,22 +3,23 @@ import dispPic from "/images/displayPicture.jpg";
 
 const DisplayPicture = ({ splashStatus, isLight }) => {
   return (
-    <div>
+    <div className="relative">
       <div
         className={`${
           splashStatus
             ? "hidden"
             : isLight
-            ? "absolute bg-highlightBrown shadowBlobFadeIn transition-all ease-in-out duration-500 md:bg-opacity-50 bg-opacity-60 blur-xl"
-            : "absolute bg-slate-800 shadowBlobFadeIn transition-all ease-in-out duration-500 blur-2xl"
-        }
-        } md:h-64 md:w-64 h-44 w-44`}
+            ? "absolute bg-highlightBrown shadowBlobFadeIn transition-all ease-in-out duration-500 md:bg-opacity-50 bg-opacity-60"
+            : "absolute bg-slate-800 shadowBlobFadeIn transition-all ease-in-out duration-500"
+        } md:h-64 md:w-64 h-44 w-44 z-10`} /* Ensure this div has a lower z-index than the image */
       ></div>
       <img
         src={dispPic}
         alt="profile picture"
         className={`${
-          splashStatus ? "" : "relative"
+          splashStatus
+            ? ""
+            : "relative z-20" /* Set higher z-index to ensure proper layering */
         } animate-blob md:h-64 md:w-64 h-44 w-44`}
       />
     </div>
