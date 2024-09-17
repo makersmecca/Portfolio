462d2926d434fd0896370534462c891caec4803e
makersmecca <ayudhnandi@gmail.com>
2024-09-17 20:31:57 +0530

    updated icon size in light mode

--- src/Components/ForkGithub.jsx
+++ src/Components/ForkGithub.jsx
@@ -24,12 +24,12 @@ const ForkGithub = ({ isLight }) => {
         <svg
           fill={`${isLight ? lightColor : darkColor}`}
           aria-hidden="true"
-          height="23"
-          width="23"
+          height={`${isLight ? "25" : "23"}`}
+          width={`${isLight ? "25" : "23"}`}
           viewBox="0 0 16 16"
           version="1.1"
           data-view-component="true"
-          className=" pt-0 pb-1 sm:pb-0 me-1 ms-1 mt-0.5"
+          className="pt-0 pb-1 sm:pb-0 me-1 ms-1 mt-0.5"
         >
           <path d="M5 5.372v.878c0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75v-.878a2.25 2.25 0 1 1 1.5 0v.878a2.25 2.25 0 0 1-2.25 2.25h-1.5v2.128a2.251 2.251 0 1 1-1.5 0V8.5h-1.5A2.25 2.25 0 0 1 3.5 6.25v-.878a2.25 2.25 0 1 1 1.5 0ZM5 3.25a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Zm6.75.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm-3 8.75a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path>
         </svg>
