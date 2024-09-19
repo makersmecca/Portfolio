46ace41ab1e35a624ada920ee259a3266feee6a7
Ayudh <ayudhnandi@gmail.com>
2024-09-19 12:51:26 +0530

    cache update

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const OFFLINE_VERSION = "1.4.8";
+const OFFLINE_VERSION = "1.4.9";
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "fallback.html";
