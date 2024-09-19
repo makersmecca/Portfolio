65964ef97992c2635f03b686445edfd533b942bf
Ayudh <ayudhnandi@gmail.com>
2024-09-19 12:05:37 +0530

    sw version update, updated asset list

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-const OFFLINE_VERSION = "1.4.6";
+const OFFLINE_VERSION = "1.4.7";
 const CACHE_NAME = "offline";
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "fallback.html";
