7a7ec4e87ce2c4dc687a0f6e4eb8b96fa521ce3c
makersmecca <ayudhnandi@gmail.com>
2024-09-20 20:24:44 +0530

    issue with adding animation to li. need to resolve conflict

--- src/Components/MyProjectDetails.jsx
+++ src/Components/MyProjectDetails.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+// import "./slideinCards.css";
 const MyProjectDetails = ({ projectDetails, isLight, splashStatus }) => {
   const arr1 = projectDetails.slice(0, projectDetails.length / 2);
   const arr2 = projectDetails.slice(
@@ -5,7 +7,30 @@ const MyProjectDetails = ({ projectDetails, isLight, splashStatus }) => {
     projectDetails.length
   );
 
-  console.log(splashStatus);
+  const projectRefs = useRef([]); // Ref to track each project card
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("slide-in");
+          }
+        });
+      },
+      { threshold: 0.1 } // Trigger animation when 10% of the card is visible
+    );
+
+    projectRefs.current.forEach((ref) => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      projectRefs.current.forEach((ref) => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, []);
 
   return (
     <>
@@ -13,20 +38,14 @@ const MyProjectDetails = ({ projectDetails, isLight, splashStatus }) => {
         <div className="col-span-2 col-start-1 text-center md:text-start md:ps-16 md:me-9 text-3xl md:text-4xl">
           Projects
         </div>
-        {/* first two projects go here */}
-        <div
-          className={`col-span-2 col-start-1 
-          ${
-            splashStatus
-              ? "hidden"
-              : "flex justify-center transition-all duration-1000 ease-in-out animate-slideIn"
-          }
-          `}
-        >
+        <div className="col-span-2 col-start-1 flex justify-center">
           <ul className="md:pt-10 pt-0">
-            {/* first two projects go here */}
-            {arr1.map((i) => (
-              <li key={i.name} className="md:pt-7 pt-5 group">
+            {arr1.map((i, index) => (
+              <li
+                key={i.name}
+                ref={(el) => (projectRefs.current[index] = el)} // Track the ref
+                className="md:pt-7 pt-5 group opacity-0 transform transition-all duration-500" // Initially hidden
+              >
                 <a
                   href={i.url}
                   target="_blank"
@@ -63,12 +82,14 @@ const MyProjectDetails = ({ projectDetails, isLight, splashStatus }) => {
             ))}
           </ul>
         </div>
-        {/* first two projects go here */}
         <div className="col-span-2 md:col-start-4 flex justify-center">
           <ul className="md:pt-10 pt-0">
-            {/* first two projects go here */}
-            {arr2.map((i) => (
-              <li key={i.name} className="md:pt-7 pt-5 rounded-lg group">
+            {arr2.map((i, index) => (
+              <li
+                key={i.name}
+                ref={(el) => (projectRefs.current[index + arr1.length] = el)} // Track the ref for the second half
+                className="md:pt-7 pt-5 group opacity-0 transform transition-all duration-500"
+              >
                 <a
                   href={i.url}
                   target="_blank"
@@ -77,7 +98,7 @@ const MyProjectDetails = ({ projectDetails, isLight, splashStatus }) => {
                     isLight
                       ? "bg-pastelRed group-hover:bg-pastelRedLight"
                       : "bg-gray-800 group-hover:bg-gray-700"
-                  }  rounded-lg`}
+                  } rounded-lg`}
                 >
                   <div className="overflow-hidden rounded-lg">
                     <img
