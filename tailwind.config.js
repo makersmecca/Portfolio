c713ed6f5685fa49186c3e1799b74e9c0b320cca
makersmecca <ayudhnandi@gmail.com>
2024-09-21 17:09:37 +0530

    added new custom animations for techstack icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,14 +17,40 @@ export default {
         pastelSlate: "#7EACB5",
       },
       animation: {
-        typing: "typing 2s steps(14), blink 0.5s infinite alternate",
+        // typing: "typing 2s steps(14), blink 0.5s infinite alternate",
         slideDown: "slideDown 1s ease-in-out forwards",
         appear: "appear 0.8s ease-in-out",
         blob: "blob 5s ease-in-out infinite",
         slideIn: "slideIn 0.2s ease-in-out forwards",
         slideOut: "slideOut 0.2s ease-in-out forwards",
+        spinSlow: "spin 3s linear infinite",
+        fastPulse: "pulse 0.8s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        wave: "wave 1s ease infinite",
+        balloonPop: "balloonPop 1.2s ease forwards",
+        rotateY: "rotateY 2s linear infinite",
       },
       keyframes: {
+        rotateY: {
+          "0%": { transform: "rotateY(0deg)" },
+          "100%": { transform: "rotateY(360deg)" },
+        },
+        wave: {
+          "0%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(15deg)",
+          },
+          "50%": {
+            transform: "rotate(-15deg)",
+          },
+          "75%": {
+            transform: "rotate(10deg)",
+          },
+          "100%": {
+            transform: "rotate(0deg)",
+          },
+        },
         slideIn: {
           "0%": { transform: "translateX(100vw)" },
           "100%": { transform: "translateX(0)" },
@@ -56,22 +82,44 @@ export default {
           "0%": { opacity: 0, transform: "translateY(-100%)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        typing: {
-          from: {
-            width: "0",
+        balloonPop: {
+          "0%": {
+            transform: "scale(1)",
+            opacity: "1",
           },
-          to: {
-            width: "3ch",
+          "50%": {
+            transform: "scale(1.3)",
+            opacity: "1",
+          },
+          "80%": {
+            transform: "scale(1.5)",
+            opacity: "1",
+          },
+          "93%": {
+            transform: "scale(1.7)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(5)",
+            opacity: "0",
           },
         },
-        blink: {
-          from: {
-            "border-color": "black",
-          },
-          to: {
-            "border-color": "white",
-          },
-        },
+        // typing: {
+        //   from: {
+        //     width: "0",
+        //   },
+        //   to: {
+        //     width: "3ch",
+        //   },
+        // },
+        // blink: {
+        //   from: {
+        //     "border-color": "black",
+        //   },
+        //   to: {
+        //     "border-color": "white",
+        //   },
+        // },
       },
     },
     fontFamily: {
