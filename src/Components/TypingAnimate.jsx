e5d696e7c7fa0b7293f5276a6feeadb1b046dcba
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2024-12-13 08:03:11 +0000

    updated installed packages and optimized code

--- src/Components/TypingAnimate.jsx
+++ src/Components/TypingAnimate.jsx
@@ -1,56 +1,32 @@
 import { TypeAnimation } from "react-type-animation";
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+const sqArr = ["Hi...", 3000, "Hello...", 1500, "Hi there...", 2500];
+const compStyle = {
+  fontSize: "2em",
+  display: "inline-block",
+  willChange: "transform",
+  transform: "translate3d(0,0,0)",
+};
 
 const TypingAnimate = ({ splashStatus, isLight }) => {
-  //   const [cursorChar, setCursorChar] = useState(true);
-
-  //   setTimeout(() => {
-  //     setCursorChar(() => !cursorChar);
-  //   }, 400);
-  // const lightColor = "#537188";
-  // const darkColor = "white";
-
-  // const [textColor, setTextColor] = useState(darkColor);
-
-  // useEffect(() => {
-  //   if (isLight) {
-  //     setTextColor(lightColor);
-  //   } else {
-  //     setTextColor(darkColor);
-  //   }
-  //   //document.getElementById("typeText").style.color = textColor;
-
-  //   return(()=>{});
-  // }, [isLight]);
-  // console.log(textColor);
-
   return (
     <div
       className={`${
         splashStatus ? "hidden" : "absolute fadeInOnly"
-      } md:text-xl text-base flex ${isLight?"text-lightColor":"text-white"}`}
-      id="typeText"
+      } md:text-xl text-base flex ${isLight ? "text-lightColor" : "text-white"}`}
     >
       <TypeAnimation
         preRenderFirstString={true}
-        sequence={["Hi...", 3000, "Hello...", 1500, "Hi there...", 2500]}
+        sequence={sqArr}
         speed={30}
         deletionSpeed={40}
         wrapper="span"
         cursor={true}
         repeat={Infinity}
-        style={{
-          fontSize: "2em",
-          display: "inline-block",
-          willChange: "transform",
-          transform: "translate3d(0,0,0)",
-        }}
-        className="self-end"
-        key = {isLight?"ligh":"dark"}
+        style={compStyle}
+        key={isLight ? "light" : "dark"}
       />
-      {/* <span style={{ fontSize: "1.8em" }}>
-        {cursorChar ? <span>/.</span> : <span>\.</span>}
-      </span> */}
     </div>
   );
 };
