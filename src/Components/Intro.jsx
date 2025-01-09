a2d89564e0592e9f49e55c651cc7d1e77159fd05
Ayudh Nandi <37550248+makersmecca@users.noreply.github.com>
2025-01-09 09:29:20 +0000

    updated the link to github profile to conditionally display at different postions based on screen size

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -83,7 +83,7 @@ const Intro = ({
                 Ayudh.
               </span>
             </div>
-            <div className="col-span-3 md:row-start-4 row-star md:col-start-3 md:text-start text-center flex justify-center md:justify-start hyphens-none px-16 sm:px-12 md:px-10 md:text-xl">
+            <div className="col-span-3 md:row-start-4 md:col-start-3 md:text-start text-center flex justify-center md:justify-start hyphens-none px-16 sm:px-12 md:px-10 md:text-xl">
               <span className="md:self-end self-center">
                 I am a Frontend developer with experience in building{" "}
                 <span className="text-newOrange">responsive,</span>{" "}
@@ -101,7 +101,7 @@ const Intro = ({
                   href="https://github.com/makersmecca"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className={`flex gap-2 items-center mt-4 md:top-2 ms-0.5 justify-center md:justify-start ${
+                  className={`sm:hidden flex gap-2 items-center mt-4 md:top-2 ms-0.5 justify-center md:justify-start ${
                     splashStatus
                       ? "opacity-0"
                       : "hover:opacity-100 opacity-75 transition-all ease-in-out"
@@ -122,6 +122,36 @@ const Intro = ({
                 <br />
                 {/* I'm eager to contribute and grow in a collaborative team. */}
               </span>
+            </div>
+            <div
+              className={`${
+                splashStatus
+                  ? "hidden"
+                  : "sm:flex sm:col-span-3 sm:col-start-3 sm:px-10"
+              } `}
+            >
+              <a
+                href="https://github.com/makersmecca"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`flex-row gap-2 items-center mt-8 ms-0.5 ${
+                  splashStatus
+                    ? "opacity-0"
+                    : "opacity-75 hover:opacity-100 transition-all ease-in-out hidden sm:flex"
+                }`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  className="bi bi-github"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+                <span className="text-lg">/makersmecca</span>
+              </a>
             </div>
           </div>
         </div>
